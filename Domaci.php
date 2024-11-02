a680f5a7b3da18c5f6301c8cf267cc3fcdbf832a
MilosJovic23 <milos.mj992@gmail.com>
2024-11-02 13:52:17 +0100

    * php homework

--- Domaci.php
+++ Domaci.php
@@ -6,9 +6,21 @@
 
 
 
+    function IznosPdva ( $cena ) {
 
+        if ( $cena < 1 ) {
+            die("broj ne moze biti manji od 1");
+        }
 
+        if ( !is_int( $cena ) ) {
+            die("broj mora biti numericka vrednost");
+        }
 
+        $pdv = $cena * 0.22;
+        echo "iznos cene za proizvod je $pdv eur";
+    }
+
+    iznosPdva(100);
 
 
 
